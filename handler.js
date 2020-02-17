be16497e5eb05a2311bb634a370a9989b1b98fc4
lomashbhattarai <lomashbhattarai1@gmail.com>
2020-02-17 22:20:27 +0545

    broker list in lambda function

--- handler.js
+++ handler.js
@@ -1,6 +1,9 @@
 'use strict';
 
-module.exports.hello = async (event, context,callback) => {
+
+
+module.exports.hello =  (event, context,callback) => {
+  
   const response =  {
     statusCode: 200,
     body: JSON.stringify(
@@ -13,3 +16,59 @@ module.exports.hello = async (event, context,callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.brokers =  (event, context, callback) => {
+  const request = require('request');
+  const cheerio = require('cheerio');
+  try {
+    request('http://www.nepalstock.com/brokers', async (error, response,html)=>{
+    if(!error && response.statusCode ==200){
+        const $ = cheerio.load(html);
+        const pager = $('.pager a').first().text();
+        const totalPages = pager.split('/')[1]
+        let brokerList = []
+        const iterableArray = [...Array(+totalPages).keys()]
+        console.log(iterableArray);
+        const promises = iterableArray.map( (i,value)=>{
+            return new Promise((resolve,reject) =>{
+                request(`http://www.nepalstock.com/brokers/index/${i+1}/`,
+            (error,response,html) =>{
+                if(!error && response.statusCode ==200){
+                    const $ = cheerio.load(html);
+                    const brokers = $('.content');    
+                    brokers.each((i,el) =>{
+                        const name = $(el).children('h4').text();
+                        const info = $(el).find('.row-content').text();
+                        let details = info.trim().split('\n');
+                        details = details.map(detail => detail.trim() )
+                        console.log(details)
+                        let [adress,code,telephone,email,website,person] = details  
+                        brokerList.push({ name,adress,code,telephone,email,website,person });
+                    })
+                    resolve(html)
+                } else {
+                    reject(error)
+                }
+            })
+            })
+        })
+        await Promise.all(promises)
+        const response =  {
+          statusCode: 200,
+          body: JSON.stringify(
+            {
+              brokers: brokerList,
+            }),
+        };
+        callback(null,response);
+    }
+  });
+
+  }
+  catch(error) {
+    //callback(error)
+  }
+  
+
+
+}
