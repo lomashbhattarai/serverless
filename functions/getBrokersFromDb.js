a5a240c14670cdc90df4efeef51e134128fb7cfe
lomashbhattarai <lomashbhattarai1@gmail.com>
2020-02-25 17:10:10 +0545

    wip

--- functions/getBrokersFromDb.js
+++ functions/getBrokersFromDb.js
@@ -1,27 +1,31 @@
 var mongoose = require("mongoose");
-mongoose.connect('mongodb+srv://lambdaUser:QgMRnjF0EzSYhj2h@cluster0-tcgij.mongodb.net/test?retryWrites=true&w=majority',
- { useNewUrlParser: true,
-useUnifiedTopology: true,
-});
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb+srv://lambdaUser:QgMRnjF0EzSYhj2h@cluster0-tcgij.mongodb.net/test?retryWrites=true&w=majority');
 var brokerModel = require('../api/schemas/broker.js');
 
 
 
 const getBrokersFromDb = (event, context,callback) => {
-    brokerModel.find({}).then((result) => {
-        const response =  {
-            statusCode: 200,
-            headers:{
-              "Access-Control-Allow-Origin": "*" 
-            },
-            body: JSON.stringify(
-              {
-                brokers: result,
-              }),
-          };
-          mongoose.connection.close();
-          callback(null,response);
-    })
+  context.callbackWaitsForEmptyEventLoop = false;
+  brokerModel.find({}).then((result) => {
+    const response =  {
+      statusCode: 200,
+      headers:{
+        "Access-Control-Allow-Origin": "*" 
+      },
+      body: JSON.stringify(
+        {
+          brokers: result,
+        })
+    };
+    
+      mongoose.connection.close();
+      callback(null,response);
+    
+    }); 
     
 };
 
